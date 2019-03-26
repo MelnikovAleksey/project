187d264fe7b9d4ce9bc4e80e14fe326769f9a26e
MelnikovAleksey <45665622+MelnikovAleksey@users.noreply.github.com>
2019-03-26 19:33:37 +0300

    Update Birzha_Thread.java

--- main/java/com/example/student4/myapplication/Birzha_Thread.java
+++ main/java/com/example/student4/myapplication/Birzha_Thread.java
@@ -1,7 +1,24 @@
 package com.example.student4.myapplication;
 
-public class Birzha_Thread extends Thread {
+
+class Birzha_Thread extends Thread {
     int vznos = 60;
     int procent = 1;
-    
+    public Birzha_Thread(String name){
+        super(name);
+    }
+    public int getID(){
+        return vznos;
+    }
+    @Override
+    public void run(){
+        while(true) {
+            try {
+                vznos += 60;
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
