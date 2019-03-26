a68f10b7ec705293f071a0963d657116238cd652
MelnikovAleksey <45665622+MelnikovAleksey@users.noreply.github.com>
2019-03-26 19:34:20 +0300

    Update MainActivity.java

--- main/java/com/example/student4/myapplication/MainActivity.java
+++ main/java/com/example/student4/myapplication/MainActivity.java
@@ -1,33 +1,31 @@
 package com.example.student4.myapplication;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Birzha_Thread t1 = new Birzha_Thread ("ttt");
     int money = 100000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        t1.start();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView textView = findViewById(R.id.textView);
         final EditText editText2 = findViewById(R.id.editText2);
-
         Button button = (Button)findViewById(R.id.Button);
         button.setOnClickListener( new View.OnClickListener() {
             double a;
             @Override
             public void onClick(View v) {
-                String S1 = editText2.getText().toString();
-                a = Double.parseDouble(S1);
-                money +=a;
+                money += t1.getID();
                 textView.setText("Tvoi den'gi: \n" + money); }
         });
+
 
     }
 }
