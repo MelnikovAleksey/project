a15ea4088505374f3fae5ceb841ef024e2728568
MelnikovAleksey <45665622+MelnikovAleksey@users.noreply.github.com>
2019-05-16 18:26:46 +0300

    Update MainActivity.java

--- main/java/com/example/myapplication/MainActivity.java
+++ main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,5 @@
-package com.example.myapplication;
+package ru.startandroid.myapplication01;
+
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -36,17 +37,26 @@ public class MainActivity extends AppCompatActivity {
     int proc_6_2 = 1;
     int proc_7_2 = 1;
     int proc_8_2 = 1;
+    int proc_1_3 = 1;
+    int proc_2_3 = 1;
+    int proc_3_3 = 1;
+    int proc_4_3 = 1;
+    int proc_5_3 = 1;
+    int proc_6_3 = 1;
+    int proc_7_3 = 1;
+    int proc_8_3 = 1;
     int kurs = 100;
     int kurs_2 = 100;
+    int kurs_3 = 100;
     Object lock = new Object();
     public void MainActivity(){
 
     }
 
     public boolean thread_change(int num){
-            if (num == 1){
-                return thr1;
-            }
+        if (num == 1){
+            return thr1;
+        }
         else if (num == 2){
             return thr2;
         }
@@ -55,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             return false;
-            }
+        }
     }
 
     public void change_thread_1(){
@@ -76,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         thr3 = true;
     }
 
-    public int procent_findout(int pr){
+    public int procent_findout_1(int pr){
 
         synchronized (lock) {
             if (pr == 1) {
@@ -101,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public int procent_findout_1(int pr){
+    public int procent_findout_2(int pr){
 
         synchronized (lock) {
             if (pr == 1) {
@@ -126,16 +136,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public int procent_findout_3(int pr){
+
+        synchronized (lock) {
+            if (pr == 1) {
+                return proc_1_3;
+            } else if (pr == 2) {
+                return proc_2_3;
+            } else if (pr == 3) {
+                return proc_3_3;
+            } else if (pr == 4) {
+                return proc_4_3;
+            } else if (pr == 5) {
+                return proc_5_3;
+            } else if (pr == 6) {
+                return proc_6_3;
+            } else if (pr == 7) {
+                return proc_7_3;
+            } else if (pr == 8) {
+                return proc_8_3;
+            } else {
+                return 0;
+            }
+        }
+    }
+
     public int money_amount(int dengivbirzhe){
         money = money - kurs*dengivbirzhe;
         return money;
     }
-    public int vnesti(int mon){
-        this.vznos = mon/kurs;
+    public int vnesti(int money){
+        this.vznos = money/kurs;
         return vznos;
     }
-    public int vivesti(int mon){
-        if(mon>vznos){
+    public int vivesti(int money){
+        if(money>vznos){
             int vivod = vznos*kurs;
             vznos = 0;
             money += vivod;
@@ -143,8 +178,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
         else {
-            int vivod = mon*kurs;
-            vznos = vznos - mon;
+            int vivod = money*kurs;
+            vznos = vznos - money;
             money += vivod;
             return vivod;
         }
@@ -166,6 +201,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("WrongCall")
             public void run() {
                 while (true) {
+                    kurs = 100;
                     Random random_procent = new Random();
                     synchronized (lock) {
                         proc_8 = proc_7;
@@ -193,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                     try {
-                        Thread.sleep(5000);
+                        Thread.sleep(10000);
 
 
                     } catch (InterruptedException e) {
@@ -207,6 +243,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("WrongCall")
             public void run() {
                 while (true) {
+                    kurs_2 = 100;
                     Random random_procent = new Random();
                     synchronized (lock) {
                         proc_8_2 = proc_7_2;
@@ -222,7 +259,7 @@ public class MainActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                textView5.setText("Процент курса: " + Integer.toString(proc_1));
+                                textView5.setText("Процент курса: " + Integer.toString(proc_1_2));
                             }
                         });
                     }
@@ -230,11 +267,54 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            textView2.setText(Integer.toString(kurs));
+                            textView2.setText(Integer.toString(kurs_2));
                         }
                     });
                     try {
-                        Thread.sleep(5000);
+                        Thread.sleep(10000);
+
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+
+        Thread thread3 = new Thread() {
+            @SuppressLint("WrongCall")
+            public void run() {
+                while (true) {
+                    kurs_3 = 100;
+                    Random random_procent = new Random();
+                    synchronized (lock) {
+                        proc_8_3 = proc_7_3;
+                        proc_7_3 = proc_6_3;
+                        proc_6_3 = proc_5_3;
+                        proc_5_3 = proc_4_3;
+                        proc_4_3 = proc_3_3;
+                        proc_3_3 = proc_2_3;
+                        proc_2_3 = proc_1_3;
+                        proc_1_3 = random_procent.nextInt(100);
+                    }
+                    if (thr3) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView5.setText("Процент курса: " + Integer.toString(proc_1_3));
+                            }
+                        });
+                    }
+                    kurs_3 *= proc_1_3;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView2.setText(Integer.toString(kurs_3));
+                        }
+                    });
+                    try {
+                        Thread.sleep(10000);
 
 
                     } catch (InterruptedException e) {
@@ -247,6 +327,7 @@ public class MainActivity extends AppCompatActivity {
 
         thread1.start();
         thread2.start();
+        thread3.start();
 
         Button button_vn = (Button) findViewById(R.id.Button_Vnesti);
         Button button_vi = (Button) findViewById(R.id.Button_Vivesti);
@@ -278,12 +359,13 @@ public class MainActivity extends AppCompatActivity {
         button_vn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String edit = editText2.getText().toString();
-                int dengivbirzhe = vnesti(Integer.parseInt(edit));
-                textView4.setText("Валюта в бирже: " + dengivbirzhe);
-                int dengi = money_amount(dengivbirzhe);
-                textView.setText("Твои деньги:  " + dengi);
+                if (thr1) {
+                    String edit = editText2.getText().toString();
+                    int money_market = vnesti(Integer.parseInt(edit));
+                    textView4.setText("Валюта в бирже: " + money_market);
+                    int money = money_amount(money_market);
+                    textView.setText("Деньги в бирже:  " + money);
+                }
             }
         });
 
@@ -292,10 +374,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String edit = editText2.getText().toString();
-                int dengivbirzhe = vivesti(Integer.parseInt(edit));
-                textView.setText("Твои деньги     " + dengivbirzhe);
-                dengivbirzhe = vznos();
-                textView4.setText("Валюта в бирже: " + dengivbirzhe);
+                int money_market = vivesti(Integer.parseInt(edit));
+                textView.setText("Твои деньги     " + money_market);
+                money_market = vznos();
+                textView4.setText("Деньги в бирже: " + money_market);
             }
         });
 
